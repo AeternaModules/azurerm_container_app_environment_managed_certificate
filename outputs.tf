b6097c91930f55525a3a6c24bb44598d6fbb9cbc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "container_app_environment_managed_certificates" {
-  description = "All container_app_environment_managed_certificate resources"
-  value       = azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates
-}
 output "container_app_environment_managed_certificates_container_app_environment_id" {
-  description = "List of container_app_environment_id values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.container_app_environment_id]
+  description = "Map of container_app_environment_id values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.container_app_environment_id }
 }
 output "container_app_environment_managed_certificates_domain_control_validation" {
-  description = "List of domain_control_validation values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.domain_control_validation]
+  description = "Map of domain_control_validation values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.domain_control_validation }
 }
 output "container_app_environment_managed_certificates_name" {
-  description = "List of name values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.name]
+  description = "Map of name values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.name }
 }
 output "container_app_environment_managed_certificates_subject_name" {
-  description = "List of subject_name values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.subject_name]
+  description = "Map of subject_name values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.subject_name }
 }
 output "container_app_environment_managed_certificates_tags" {
-  description = "List of tags values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.tags]
+  description = "Map of tags values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.tags }
 }
 output "container_app_environment_managed_certificates_validation_token" {
-  description = "List of validation_token values across all container_app_environment_managed_certificates"
-  value       = [for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : v.validation_token]
+  description = "Map of validation_token values across all container_app_environment_managed_certificates, keyed the same as var.container_app_environment_managed_certificates"
+  value       = { for k, v in azurerm_container_app_environment_managed_certificate.container_app_environment_managed_certificates : k => v.validation_token }
 }
 
